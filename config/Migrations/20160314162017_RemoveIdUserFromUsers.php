fc54061d3fc13867205b819bb2abe36994f108e2
eddy gutierrez soria <eddy gutierrez soria>
2016-03-14 14:14:25 -0400

    cinema

---
+++ config/Migrations/20160314162017_RemoveIdUserFromUsers.php
@@ -0,0 +1,19 @@
+<?php
+use Migrations\AbstractMigration;
+
+class RemoveIdUserFromUsers extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->removeColumn('Id_user');
+        $table->update();
+    }
+}
